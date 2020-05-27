96bf8b4066ec09ab9286c200c8ae71ce74bf144e
Seba <sesepulveda@ug.uchile.cl>
2020-05-27 03:07:58 -0400

    init project

---
+++ revision_alumnos.py
@@ -0,0 +1,14 @@
+'''
+	Con el siguiente comando se crean directorios según
+	archivo zip de archivos de alumnos:
+
+	revision_alumnos -rz <file.zip>
+
+	Además se crea un archivo json que contendrá:
+
+	Alumnos: {
+		name_alumno: <str>,
+		comentario: <str>
+	}
+
+'''
